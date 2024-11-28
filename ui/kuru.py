905139c8044c19fc171f7b664b235d60925bc35c
Likhit Bhogadi <likhitbhogadi@gmail.com>
2024-11-29 03:31:45 +0530

    added print_menu.py - modular

--- ui/kuru.py
+++ ui/kuru.py
@@ -1,8 +1,4 @@
 from rich.console import Console
-# from rich.panel import Panel
-# from rich.layout import Layout
-# from rich.table import Table
-# from rich.prompt import Prompt
 import subprocess as sp
 import pymysql
 import pymysql.cursors
@@ -22,36 +18,9 @@ from delete_event import delete_event_and_related_data
 from formation_warrior import get_formation_and_warrior_names
 from add_formation import add_new_formation
 from modify_battle_form import modify_battle_formation
-
-# def print_banner():
-#     """Prints a colorful banner."""
-#     print(Fore.LIGHTCYAN_EX+ Style.BRIGHT)
-#     print("=======================================================")
-#     print("               WELCOME TO COMPANY DB TOOL              ")
-#     print("=======================================================")
-#     print(Style.RESET_ALL)
+from print_menu import print_menu   
 
 console = Console()
-
-def print_menu():
-    """Prints the main menu with colors."""
-    print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "MAIN MENU:")
-    print(Style.RESET_ALL)
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "1." + Style.RESET_ALL + " Get warrior details")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "2." + Style.RESET_ALL + " Get Details of Divine weapon ")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "3." + Style.RESET_ALL + " Get Battle Formation Details")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "4." + Style.RESET_ALL + " Get Kinngdom Details")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "5." + Style.RESET_ALL + " Get Total Strength of Anga_nama")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "6." + Style.RESET_ALL + " Get warrior Details on partial or full name")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "7." + Style.RESET_ALL + " Get list of divine weapon with warrior based on kingdom")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "8." + Style.RESET_ALL + " Get formation name and warrior Id based on kingdom name")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "9." + Style.RESET_ALL + " Add a new warrior")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "10." + Style.RESET_ALL + " Modify a Battle formation based on Day and army_id")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "11." + Style.RESET_ALL + " Delete Event from Yuddhahani")
-    print(Fore.LIGHTYELLOW_EX+ Style.BRIGHT + "12." + Style.RESET_ALL + " Add a new Battle formation")
-    print(Fore.LIGHTRED_EX+ Style.BRIGHT + "14." + Style.RESET_ALL + " Logout")
-    print(Fore.LIGHTCYAN_EX+ Style.BRIGHT + "-" * 50 + Style.RESET_ALL)
-
 
 def Delete_warrior():
     print("delete warrior")
